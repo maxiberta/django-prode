1e076a5e696d7a731ef40afaeafdcc65d57d18bb
Maximiliano Bertacchini <maxiberta@gmail.com>
2011-07-25 17:49:17 -0300

    file restructuring to make a proper django app

--- django_prode/urls.py
+++ django_prode/urls.py
@@ -1,17 +1,6 @@
-from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'copa_america.views.home', name='home'),
-    # url(r'^copa_america/', include('copa_america.foo.urls')),
-    url(r'^$', include('fixture.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+        (r'^$','fixture.views.index',{},'frontpage'),
 )
+
